ed7320aed1989a9be9f08561eeed703a91715a93
= <=>
2019-05-03 13:15:00 +0200

    exception handling corrected 2

--- src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
+++ src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
@@ -31,9 +31,9 @@ public class TorpedoStore {
     }
   }
 
-  public boolean fire(int numberOfTorpedos) throws IllegalArgumentException{
+  public boolean fire(int numberOfTorpedos) {
     if(numberOfTorpedos < 1 || numberOfTorpedos > this.torpedoCount){
-      new IllegalArgumentException("numberOfTorpedos");
+      throw new IllegalArgumentException("numberOfTorpedos");
     }
 
     boolean success = false;
